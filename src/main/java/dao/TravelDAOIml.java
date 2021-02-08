b6c9f8120dc2616fec0f259a0f9c4dfad2c95627
efsam <E.salimi75@ymail.com>
2021-02-08 18:35:26 +0330

    commit TravelDAOIml.java to secondary

--- src/main/java/dao/TravelDAOIml.java
+++ src/main/java/dao/TravelDAOIml.java
@@ -6,6 +6,7 @@ import entity.Travel;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
+import java.sql.Date;
 import java.util.List;
 
 
@@ -14,6 +15,8 @@ public class TravelDAOIml implements AbstractTravelDAO {
     private JdbcTemplate jdbcTemplate;
 
     private final String SQL_FIND_ALL_CITIES = "select * from city";
+    private final String SQL_FIND_TRAVEL_BY_DATE =
+            "select * from travel where origin=? and destination=? and departure_date=?";
 
     public TravelDAOIml(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -21,11 +24,13 @@ public class TravelDAOIml implements AbstractTravelDAO {
 
     @Override
     public List<City> getAllCities() {
-        return jdbcTemplate.query(SQL_FIND_ALL_CITIES, new CityMapper());
+        return this.jdbcTemplate.query(SQL_FIND_ALL_CITIES, new CityMapper());
     }
 
     @Override
-    public List<Travel> getTravelByDate() {
-        return null;
+    public List<Travel> getTravelByDate(Integer origin, Integer destination, Date date) {
+        return this.jdbcTemplate.query(SQL_FIND_TRAVEL_BY_DATE,new Object[]{origin, destination, date}, new TravelMapper());
     }
+
+
 }
