8f453f1d69b704ab32eb644fa8cd9713d08a9b99
efsam <E.salimi75@ymail.com>
2021-02-08 20:32:52 +0330

    commit Ticket.java to secondary

--- src/main/java/entity/Ticket.java
+++ src/main/java/entity/Ticket.java
@@ -6,4 +6,54 @@ public class Ticket {
     private String passengerName;
     private String gender;
     private String ticketId;
+    private Integer travelId;
+    private Integer user_id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public Integer getTravelId() {
+        return travelId;
+    }
+
+    public void setTravelId(Integer travelId) {
+        this.travelId = travelId;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
 }
