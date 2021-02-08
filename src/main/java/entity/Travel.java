25fd1c357b434287ef1fac9d7c21cd79fbe6f02d
efsam <E.salimi75@ymail.com>
2021-02-08 16:05:29 +0330

    commit Travel.java to secondary

---
+++ src/main/java/entity/Travel.java
@@ -0,0 +1,10 @@
+package entity;
+
+import java.util.Date;
+
+public class Travel {
+    private Integer id;
+    private Date departureDate;
+    private String departureTime;
+    private String travelId;
+}
