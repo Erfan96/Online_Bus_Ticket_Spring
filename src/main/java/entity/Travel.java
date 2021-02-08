681eb010e8c18ad1f1b4eab2a7fe7a25d007c4d8
efsam <E.salimi75@ymail.com>
2021-02-08 17:47:40 +0330

    commit Travel.java to secondary

--- src/main/java/entity/Travel.java
+++ src/main/java/entity/Travel.java
@@ -8,4 +8,36 @@ public class Travel {
     private Date departureDate;
     private String departureTime;
     private String travelId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(Date departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getTravelId() {
+        return travelId;
+    }
+
+    public void setTravelId(String travelId) {
+        this.travelId = travelId;
+    }
 }
