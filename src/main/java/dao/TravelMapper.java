a23f3de34f4db73a79fa139a94f7ea16266116e4
efsam <E.salimi75@ymail.com>
2021-02-08 18:36:09 +0330

    commit TravelMapper.java to secondary

---
+++ src/main/java/dao/TravelMapper.java
@@ -0,0 +1,19 @@
+package dao;
+
+import entity.Travel;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TravelMapper implements RowMapper<Travel> {
+
+    @Override
+    public Travel mapRow(ResultSet resultSet, int i) throws SQLException {
+        Travel travel = new Travel();
+        travel.setDepartureDate(resultSet.getDate("departure_date"));
+        travel.setDepartureTime(resultSet.getString("departure_time"));
+        travel.setTravelId(resultSet.getString("travel_id"));
+        return travel;
+    }
+}
