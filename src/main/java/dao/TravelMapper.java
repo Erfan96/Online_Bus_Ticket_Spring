986c3637613657bebec0b5235f8458b783019e70
efsam <E.salimi75@ymail.com>
2021-02-09 07:08:43 +0330

    commit TravelMapper.java in secondary

--- src/main/java/dao/TravelMapper.java
+++ src/main/java/dao/TravelMapper.java
@@ -11,6 +11,7 @@ public class TravelMapper implements RowMapper<Travel> {
     @Override
     public Travel mapRow(ResultSet resultSet, int i) throws SQLException {
         Travel travel = new Travel();
+        travel.setId(resultSet.getInt("id"));
         travel.setDepartureDate(resultSet.getDate("departure_date"));
         travel.setDepartureTime(resultSet.getString("departure_time"));
         travel.setTravelId(resultSet.getString("travel_id"));
