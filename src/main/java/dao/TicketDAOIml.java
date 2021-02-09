e769b28bfedcd60550e86ad0dcf1081c4dbad36c
efsam <E.salimi75@ymail.com>
2021-02-09 15:41:30 +0330

    commit TicketDAOIml.java in secondary

--- src/main/java/dao/TicketDAOIml.java
+++ src/main/java/dao/TicketDAOIml.java
@@ -9,8 +9,9 @@ public class TicketDAOIml implements AbstractTicketDAO {
 
     private JdbcTemplate jdbcTemplate;
 
-    private final String SQL_BUY_TICKET = "insert into ticket " +
+    private final String SQL_ADD_TICKET = "insert into ticket " +
             "(gender, passenger_name, ticket_id, travel_id, client_user_id) values(?, ?, ?, ?, ?)";
+    private final String SQL_DELETE_TICKET_BY_TICKET_ID = "delete from ticket where ticket_id=?";
 
 
     public TicketDAOIml(DataSource dataSource) {
@@ -18,9 +19,14 @@ public class TicketDAOIml implements AbstractTicketDAO {
     }
 
     @Override
-    public boolean buyTicket(Ticket ticket) {
+    public boolean addTicket(Ticket ticket) {
         return this.jdbcTemplate.update
-         (SQL_BUY_TICKET, ticket.getGender(), ticket.getPassengerName(),
+         (SQL_ADD_TICKET, ticket.getGender(), ticket.getPassengerName(),
                  ticket.getTicketId(), ticket.getTravelId(), ticket.getUser_id()) > 0;
+    }
+
+    @Override
+    public boolean deleteTicketByTicketId(String ticketID) {
+        return this.jdbcTemplate.update(SQL_DELETE_TICKET_BY_TICKET_ID, ticketID) > 0;
     }
 }
