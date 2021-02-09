e463af44d93bb376c4ccc447abd134a93cc18baf
efsam <E.salimi75@ymail.com>
2021-02-09 09:37:53 +0330

    commit DetailTicketService.java in secondary

---
+++ src/main/java/service/DetailTicketService.java
@@ -0,0 +1,19 @@
+package service;
+
+import dao.AbstractDetailTicketDAO;
+import entity.DetailTicket;
+
+import java.util.List;
+
+public class DetailTicketService {
+
+    private AbstractDetailTicketDAO abstractDetailTicketDAO;
+
+    public DetailTicketService(AbstractDetailTicketDAO abstractDetailTicketDAO) {
+        this.abstractDetailTicketDAO = abstractDetailTicketDAO;
+    }
+
+    public List<DetailTicket> getListOfTicketsByUserID(Integer id) {
+        return this.abstractDetailTicketDAO.getListOfTicketsByUserID(id);
+    }
+}
