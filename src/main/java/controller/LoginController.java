e909bd4386e5cff9108ca59b2a0e048d95be185b
efsam <E.salimi75@ymail.com>
2021-02-10 11:28:28 +0330

    commit LoginController.java in secondary

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import entity.User;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+import service.LoginService;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.PrintWriter;
+
+public class LoginController extends AbstractController {
+
+    private LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        String username = httpServletRequest.getParameter("userName");
+        String password = httpServletRequest.getParameter("password");
+        User user = loginService.getUserByUsername(username);
+        HttpSession session = httpServletRequest.getSession();
+        session.setAttribute("use", user);
+
+        if (user.getUserName().equals(username) && user.getPassword().equals(password)) {
+            Cookie use = new Cookie("user", username);
+            Cookie pass = new Cookie("pass", password);
+            httpServletResponse.addCookie(use);
+            httpServletResponse.addCookie(pass);
+            httpServletRequest.getRequestDispatcher("/search").forward(httpServletRequest, httpServletResponse);
+        }
+        else {
+            return new ModelAndView("login");
+        }
+
+        return null;
+    }
+}
