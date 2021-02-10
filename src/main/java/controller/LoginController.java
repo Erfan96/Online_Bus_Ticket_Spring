12c3f261b87a49e12bb5ce0283b8762e5d4e94d7
efsam <E.salimi75@ymail.com>
2021-02-10 19:15:58 +0330

    commit LoginController.java in main

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.PrintWriter;
+
 
 public class LoginController extends AbstractController {
 
